fb4ef821e6385388a70c1a0d3e02b588e1dcbf90
Extra.#Flame <13707331836@163.com>
2025-11-26 02:00:40 +0800

    初始化仓库并添加已有源码和成品文件

---
+++ ppm/src/main.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main() {
+    const char* output_path = "output_cpp.ppm";
+    const int width = 16 * 60;
+    const int height = 9 * 60;
+
+    std::ofstream ofs("output_cpp.ppm", std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            unsigned char r = (unsigned char)((float)x / width * 255.0f);
+            unsigned char g = (unsigned char)((float)y / height * 255.0f);
+            unsigned char b = 50;
+
+            ofs << r << g << b;
+        }
+    }
+
+    ofs.close();
+    std::cout << "PPM File Generated: " << output_path << std::endl;
+
+    return 0;
+}
